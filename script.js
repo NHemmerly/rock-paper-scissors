34151a5165534d6f497145427e5956d2afa0db01
Neil H <neilhemm0@gmail.com>
2022-11-12 23:58:58 -0500

    Write function for making the user's answer comparable

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 //Random number generator selects a number from 0 to 2
 function getComputerAnswer() {
     let answer = Math.floor(Math.random() * 3);
-    let answerWord = '';
-
-    if (answer === 0) 
-    {
-        answerWord = 'rock';
-    }
-    else if (answer === 1) 
-    {
-        answerWord = 'paper';
-    }
-    else 
-    {
-        answerWord = 'scissors';
-    }
          
-    return answerWord;
+    return answer;
+}
+
+function getUserAnswer(promptAnswer) {
+    if (typeof promptAnswer === 'string') {
+        promptAnswer = promptAnswer.toLowerCase();
+
+        if (promptAnswer === 'rock') 
+        {
+            promptAnswer = 0;
+        }
+        else if (promptAnswer === 'paper') 
+        {
+            promptAnswer = 1;
+        }
+        else{
+            promptAnswer = 2;
+        }
+
+        return promptAnswer;
+    }
+
+    console.log("Please enter either 'rock', 'paper', or 'scissors.'")
+    
 }
 
 console.log(getComputerAnswer());
